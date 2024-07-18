97bfa1b7dec6677df8dbff2bfda2efd5c64a619b
akram1903 <akram.a.e.2002@gmail.com>
2024-07-18 11:44:31 +0300

    separating every button into a separate function

--- pages/login.py
+++ pages/login.py
@@ -23,13 +23,23 @@ class LoginPage:
     # Interaction Methods
 
     def load(self):
-        self.browser.get(self.URL)
+        self.browser.get(self.base_url)
 
     def login(self, usr,pswd):
+        self.enter_username(usr)
+        self.enter_password(pswd)
+        self.submit_form()
+     
+
+    def enter_username(self,usr):
         user_input = self.browser.find_element(*self.username_input)
         user_input.send_keys(usr)
+        
+    def enter_password(self,pswd):
         pass_input = self.browser.find_element(*self.password_input)
         pass_input.send_keys(pswd)
+        
+    def submit_form(self):
         submit = self.browser.find_element(*self.submit)
         submit.send_keys(Keys.ENTER)
      
